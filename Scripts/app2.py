34bc13312c2f080e44e0e781ef75366faa494f55
tsakas96 <aristos_fox@hotmail.com>
2021-04-27 18:50:52 +0200

    session state update

--- Scripts/app2.py
+++ Scripts/app2.py
@@ -124,8 +124,8 @@ def main():
     menu = ["Home", "Login", "Signup"]
     choice = st.sidebar.selectbox("Menu", menu)
     session_state = SessionState.get(user_logged_in = False, user_id = "", state = "Start", history=0)
-    icon_info = np.load("./Scripts/icon_info1.npy", allow_pickle=True)
-    #icon_info = np.load("icon_info1.npy", allow_pickle=True)
+    #icon_info = np.load("./Scripts/icon_info1.npy", allow_pickle=True)
+    icon_info = np.load("icon_info1.npy", allow_pickle=True)
 
     st.title("Draw the sketch!")
     st.markdown(' ')
